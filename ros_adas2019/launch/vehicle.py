bc20d75b365a1f4ce32863bafb31795b2a13b0a6
Hermann Höhne <hoe@fh-wedel.de>
2021-08-25 13:44:22 +0200

    Switch to ROS2 compatible pylon camera node.

--- ros_adas2019/launch/vehicle.py
+++ ros_adas2019/launch/vehicle.py
@@ -1,31 +1,58 @@
 """Launch the entire vehicle: ADAS2019 actuators and sensors plus lidar and camera."""
 
 import launch
+from launch_ros.actions import Node
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions.this_launch_file_dir import ThisLaunchFileDir
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    ros_adas2019 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/ros_adas2019.py'])
+    )
+    
+    rplidar = Node(
+        name = 'rplidar',
+        namespace = 'lidar',
+        package = 'rplidar_ros',
+        executable = 'rplidar_composition',
+        output = 'screen'
+    )
+    laser_frame = Node(
+        name = 'laser_frame',
+        namespace = 'lidar',
+        package = 'tf2_ros', 
+        executable = "static_transform_publisher",
+        arguments = "0.45 0 0 0 0 1 0 base_link laser".split()
+    )
+    
     """Use composition for all image-processing nodes.
     
-    Keeps overhead low sine image data can reside in shared memory."""
+    Keeps overhead low since image data can – theoretically – reside in shared memory."""
     image_processing = ComposableNodeContainer(
-            name='image_processing',
-            namespace='pylon_camera_node',
-            package='rclcpp_components',
-            executable='component_container',
-            composable_node_descriptions=[
+            name = 'container',
+            namespace = 'pylon_camera_node',
+            package = 'rclcpp_components',
+            executable = 'component_container',
+            composable_node_descriptions = [
                 ComposableNode(
-					name='pylon_camera',
-					namespace='pylon_camera_node',
-                    package='pylon_usb_instant_camera',
-                    plugin='pylon_usb_instant_camera::PylonUSBCameraNode'),
+                    name = 'pylon_camera',
+                    namespace = 'pylon_camera_node',
+                    package = 'pylon_instant_camera',
+                    plugin = 'pylon_instant_camera::PylonCameraNode',
+                    parameters = [{'camera_settings_pfs': get_package_share_directory('ros_adas2019')+'/config/rgb8.pfs'}]
+                ),
                 ComposableNode(
-					name='pylon_camera_rectify',
-					namespace='pylon_camera_node',
-                    package='image_proc',
-                    plugin='image_proc::RectifyNode')
+                    name = 'pylon_camera_rectify',
+                    namespace = 'pylon_camera_node',
+                    package = 'image_proc',
+                    plugin = 'image_proc::RectifyNode'
+                )
             ],
-            output='screen',
+            output = 'screen'
     )
 
-    return launch.LaunchDescription([image_processing])
+    return launch.LaunchDescription([rplidar, laser_frame, image_processing, ros_adas2019])
