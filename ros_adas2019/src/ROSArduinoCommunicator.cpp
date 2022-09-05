e840eeb912d3d71f35f630eb1b1ed46563fc4425
Hermann Höhne <hoe@fh-wedel.de>
2022-09-05 17:34:57 +0200

    Try more than once to connect to Arduino.

--- ros_adas2019/src/ROSArduinoCommunicator.cpp
+++ ros_adas2019/src/ROSArduinoCommunicator.cpp
@@ -1,17 +1,29 @@
 #include "ROSArduinoCommunicator.hpp"
 
 ROSArduinoCommunicator::ROSArduinoCommunicator(ARDUINO_ID arduinoId) : rclcpp::Node::Node("adas_2019") {
-    if (!arduinoComClient.init(arduinoId, SERIAL_DEVICE_PREFIX, NUM_ARDUINO)) {
+    bool connected = false;
+    const unsigned char max_tries = 3;
+    for (unsigned char tries = 1; !connected && tries <= max_tries; tries++) {
+        try {
+            connected = arduinoComClient.init(arduinoId, SERIAL_DEVICE_PREFIX, NUM_ARDUINO);
+        } catch (const boost::wrapexcept<boost::system::system_error> &e) {
+            RCLCPP_WARN(this->get_logger(),
+                "Exception while initializing arduino communication: %s",
+                 e.what()
+            );
+        }
+    }
+    if (!connected) {
         RCLCPP_ERROR(this->get_logger(),
-			"Unable to find arduino with id %d.",
-			 arduinoId
-		);
+            "Unable to find arduino with id %d.",
+             arduinoId
+        );
         throw std::runtime_error("Unable to connect to arduino.");
     } else {
         RCLCPP_INFO(this->get_logger(),
-			"Connected to arduino on port:\t\t\t%d\t ID: %d\t Software version: %d",
-			 arduinoComClient.get_port_num(), arduinoComClient.get_id(), arduinoComClient.get_software_version()
-		);
+            "Connected to arduino on port: %d\t ID: %d\t Software version: %d",
+             arduinoComClient.get_port_num(), arduinoComClient.get_id(), arduinoComClient.get_software_version()
+        );
     }
 }
 
@@ -36,21 +48,21 @@ void ROSArduinoCommunicator::triggerUpdate() {
 
         switch (id) {
             case ID_ARD_SENSOR_INFO:
-				RCLCPP_INFO(this->get_logger(),
-					"Info frame received.\tID: %u\tSoftware version: %u", data.info.ui8ArduinoAddress,
-					 data.info.ui16ArduinoVersion
-				);
+                RCLCPP_INFO(this->get_logger(),
+                    "Info frame received.\tID: %u\tSoftware version: %u", data.info.ui8ArduinoAddress,
+                     data.info.ui16ArduinoVersion
+                );
             break;
 
             case ID_ARD_SENS_ERROR:
-				RCLCPP_ERROR(this->get_logger(),
-					"Error frame received from unit: %d", arduinoComClient.get_id()
-				);
-			break;
+                RCLCPP_ERROR(this->get_logger(),
+                    "Error frame received from unit: %d", arduinoComClient.get_id()
+                );
+            break;
 
             default:
                 onDataReceived(id, timestamp, data);
-			break;
+            break;
         }
     }
 }
